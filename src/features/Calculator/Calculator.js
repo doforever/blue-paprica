3259954a4f730cf2560c6283ad3a2b8abb1cb66d
Dorota Tomala <doforever@gmail.com>
2022-03-16 18:06:57 +0100

    Calculate result no input validation

--- src/features/Calculator/Calculator.js
+++ src/features/Calculator/Calculator.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import './Calculator.module.scss';
 
 function Calculator() {
+  const [bins, setBins] = useState(20);
+  const [cost, setCost] = useState(20);
+
+  const getResult = () => (
+    12 * (bins * cost - bins * cost / 5)
+  );
+
   return (
     <section className="calculator">
-      Calculator
+      <h2>Ile zaoszczędzisz?</h2>
+      <p>Rachunek jest prosty.</p>
+      <form>
+        <div>
+          <label htmlFor="bins">Ile pojemników na szkło zapełniasz miesięcznie?</label>
+          <input id="bins" name="bins" type="text" value={bins} onChange={({ target: { value } }) => setBins(value)}/>
+        </div>
+        <div>
+          <label htmlFor="cost">Ile płacisz za wywóz jednego pojemnika na szkło?</label>
+          <input id="cost" name="cost" type="text" value={cost} onChange={({ target: { value } }) => setCost(value)}/>
+        </div>
+        <div>
+          Dzięki kruszarce zaoszczędzisz <span>{getResult()}</span> rocznie.
+        </div>
+      </form>
     </section>
   );
 }
