ff9041745c4b4b776dafd3762a8fe6c75eb6941b
Dorota Tomala <doforever@gmail.com>
2022-03-16 21:53:29 +0100

    Validate calculator inputs

--- src/features/Calculator/Calculator.js
+++ src/features/Calculator/Calculator.js
@@ -6,8 +6,20 @@ function Calculator() {
   const [cost, setCost] = useState(20);
 
   const getResult = () => (
-    12 * (bins * cost - bins * cost / 5)
+    (12 * (bins * cost - bins * cost / 5)).toFixed()
   );
+
+  const changeBinInput = ({ target: { value } }) => {
+    if (!isNaN(value) && value >= 0) {
+      setBins(value);
+    }
+  };
+
+  const changeCostInput = ({ target: { value } }) => {
+    if (!isNaN(value) && value >= 0) {
+      setCost(value);
+    }
+  };
 
   return (
     <section className="calculator">
@@ -16,14 +28,14 @@ function Calculator() {
       <form>
         <div>
           <label htmlFor="bins">Ile pojemników na szkło zapełniasz miesięcznie?</label>
-          <input id="bins" name="bins" type="text" value={bins} onChange={({ target: { value } }) => setBins(value)}/>
+          <input id="bins" name="bins" type="text" value={bins} onChange={changeBinInput}/>
         </div>
         <div>
           <label htmlFor="cost">Ile płacisz za wywóz jednego pojemnika na szkło?</label>
-          <input id="cost" name="cost" type="text" value={cost} onChange={({ target: { value } }) => setCost(value)}/>
+          <input id="cost" name="cost" type="text" value={cost} onChange={changeCostInput}/>
         </div>
         <div>
-          Dzięki kruszarce zaoszczędzisz <span>{getResult()}</span> rocznie.
+          Dzięki kruszarce zaoszczędzisz <span>{getResult()} zł</span> rocznie.
         </div>
       </form>
     </section>
